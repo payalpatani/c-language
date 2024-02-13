d5b9289fa14f01e64a729de5ac86dab628bf8bc7
payalpatani <159900457+payalpatani@users.noreply.github.com>
2024-02-13 17:04:53 +0530

    Add files via upload

---
+++ task.c
@@ -0,0 +1,4 @@
+# include<stdio.h>
+main(){
+		printf(" \t MY NAME IS :PAYAL\n \t MY AGE IS : 21 YEAR\n\t MY SCHOOL NAME IS : SUPER HIGH SCHOOL");
+}
---
+++ task2.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+main(){
+		printf("------\n");
+		printf("|    |\n");
+		printf("R    |\n");
+		printf("N    |\n");
+		printf("W    |\n");
+		printf("|    |\n");
+		printf("------");
+		
+}
