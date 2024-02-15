0de06a03ab7c76c47089d6b46c2a1f49229d1dc0
payalpatani <159900457+payalpatani@users.noreply.github.com>
2024-02-15 14:00:43 +0530

    Add files via upload

---
+++ Greenpro.c
@@ -0,0 +1,81 @@
+// QUE::--1 Write a Program to convert temperature from degrees Celsius to Fahrenheit.
+
+// #include <stdio.h>
+
+// int main() {
+//     float celsius, fahrenheit;
+
+//     printf("Enter temperature in Celsius: ");
+//     scanf("%f", &celsius);
+
+//     fahrenheit = (celsius * 9/5) + 32;
+
+//     printf("%.2f Celsius is equal to %.1f Fahrenheit\n", celsius, fahrenheit);
+
+//     return 0;
+// }
+
+
+
+
+
+
+// QUE ::--2 Write C Program to find gross salary by adding % of HRA, DA, and TA of user choice.
+
+// #include <stdio.h>
+
+// int main() {
+//     float basicSalary, hraPercent, daPercent, taPercent;
+//     float grossSalary;
+
+//     printf("Enter basic salary: ");
+//     scanf("%f", &basicSalary);
+
+//     printf("Enter percentage of (HRA): ");
+//     scanf("%f", &hraPercent);
+
+//     printf("Enter percentage of (DA): ");
+//     scanf("%f", &daPercent);
+
+//     printf("Enter percentage of (TA): ");
+//     scanf("%f", &taPercent);
+
+//     float hra = basicSalary * (hraPercent / 100);
+//     float da = basicSalary * (daPercent / 100);
+//     float ta = basicSalary * (taPercent / 100);
+
+//     grossSalary = basicSalary + hra + da + ta;
+
+//     printf("Gross salary: %.2f\n", grossSalary);
+
+//     return 0;
+// }
+
+
+
+
+
+
+// QUE ::--3  Write C Program to find the third angle of a right triangle if two other angles are given.
+
+// #include <stdio.h>
+
+// int main() {
+//     float angle1, angle2, angle3;
+
+//     // Input the measures of the two angles
+//     printf("Enter the first angle: ");
+//     scanf("%f", &angle1);
+
+//     printf("Enter the  second angle: ");
+//     scanf("%f", &angle2);
+
+//     // Calculate the third angle
+//     angle3 = 180 - angle1 - angle2;
+
+//     // Display the third angle
+//     printf(" The third angle is: %.1f degrees\n", angle3);
+
+//     return 0;
+// }
+
