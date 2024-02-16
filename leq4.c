002552f232e08d8b6db57759e04c8079a4077941
payalpatani <159900457+payalpatani@users.noreply.github.com>
2024-02-16 13:36:44 +0530

    Add files via upload

---
+++ leq4.c
@@ -0,0 +1,141 @@
+// QUE ::-- 1 Write a Program to Swap two variables using the third variable.
+
+// #include <stdio.h>
+
+// int main() {
+//     int a, b, temp;
+
+//     printf("Enter value of a: ");
+//     scanf("%d", &a);
+
+//     printf("Enter value of b: ");
+//     scanf("%d", &b);
+
+//     printf("Before swapping:\n");
+//     printf("a = %d\n", a);
+//     printf("b = %d\n", b);
+
+//     temp = a;
+//     a = b;
+//     b = temp;
+
+//     printf("After swapping:\n");
+//     printf("a = %d\n", a);
+//     printf("b = %d\n", b);
+
+//     return 0;
+// }
+
+
+
+
+
+// QUE ::--2 Write a Program to Swap two variables without using a third variable.
+
+// #include <stdio.h>
+
+// int main() {
+//     int a, b;
+
+//     printf("Enter value of a: ");
+//     scanf("%d", &a);
+
+//     printf("Enter value of b: ");
+//     scanf("%d", &b);
+
+//     printf("Before swapping:\n");
+//     printf("a = %d\n", a);
+//     printf("b = %d\n", b);
+
+//     // swapping without using a third variable
+//     a = a + b;
+//     b = a - b;
+//     a = a - b;
+
+//     printf("After swapping:\n");
+//     printf("a = %d\n", a);
+//     printf("b = %d\n", b);
+
+//     return 0;
+// }
+
+
+
+
+
+// QUE ::--3  Write a Program to find the formula's answer (x+y)^2.
+
+// #include <stdio.h>
+
+// int main() {
+//     int x, y;
+//     int result;
+
+//     printf("Enter the value of x: ");
+//     scanf("%d", &x);
+
+//     printf("Enter the value of y: ");
+//     scanf("%d", &y);
+
+//     
+//     result = (x + y) * (x + y);
+
+//     printf("The result of (%d + %d)^2 is: %d\n", x, y, result);
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+// QUE ::--4 Write a Program to find the formula's answer (x-y)^2.
+
+// #include <stdio.h>
+
+// int main() {
+//     int x, y;
+//     int result;
+
+//     printf("Enter the value of x: ");
+//     scanf("%d", &x);
+
+//     printf("Enter the value of y: ");
+//     scanf("%d", &y);
+
+   
+//     result = (x - y) * (x - y);
+
+//     printf("The result of (%d - %d)^2 is: %d\n", x, y, result);
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+// QUE ::--5  Write a Program to find the formula's answer (x+y)^3
+
+// #include <stdio.h>
+
+// int main() {
+//     int x, y;
+//     int result;
+
+//     printf("Enter the value of x: ");
+//     scanf("%d", &x);
+
+//     printf("Enter the value of y: ");
+//     scanf("%d", &y);
+
+//     result = (x + y) * (x + y) * (x + y);
+
+//     printf("The result of (%d + %d)^3 is: %d\n", x, y, result);
+
+//     return 0;
+// }
