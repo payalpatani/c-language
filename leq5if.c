6b275a8fb2e33e5d9c7d50b1c1116422f2b4db85
payalpatani <159900457+payalpatani@users.noreply.github.com>
2024-02-16 14:20:06 +0530

    Add files via upload

---
+++ leq5if.c
@@ -0,0 +1,46 @@
+// QUE ::--1 Write a Program to find the minimum number from the given 2 numbers using if else.
+
+// #include <stdio.h>
+
+// int main() {
+//     int num1, num2;
+
+//     printf("Enter the first number: ");
+//     scanf("%d", &num1);
+//     printf("Enter the second number: ");
+//     scanf("%d", &num2);
+    
+//     if (num1 < num2) {
+//         printf("The minimum number is: %d\n", num1);
+//     } else {
+//         printf("The minimum number is: %d\n", num2);
+//     }
+    
+//     return 0;
+// }
+
+
+
+
+// QUE ::--2  Write a Program to find if a given number is neutral or not using a ladder if else.
+
+// #include <stdio.h>
+
+// int main() {
+//     int number;
+
+//     // Input the number
+//     printf("Enter a number: ");
+//     scanf("%d", &number);
+    
+    
+//     if (number > 0) {
+//         printf("%d is a positive number.\n", number);
+//     } else if (number < 0) {
+//         printf("%d is a negative number.\n", number);
+//     } else {
+//         printf("%d is a neutral number (zero).\n", number);
+//     }
+
+//     return 0;
+// }
